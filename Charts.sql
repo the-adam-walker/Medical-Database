20de64f85b93cc3acc4193f6436fd7866e2c56e9
danieljpietz <danieljpietz@gmail.com>
2019-11-22 16:07:56 -0700

    Added Charts.sql

---
+++ Charts.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Charts (
+  `Gender` varchar(6) NOT NULL,
+  `First_Name` varchar(20) NOT NULL,
+  `Surname` varchar(23) NOT NULL,
+  `DOB` varchar(10) NOT NULL,
+  `Height` varchar(3) NOT NULL,
+  `Weight` varchar(5) NOT NULL,
+  `Nurse` varchar(50) NOT NULL,
+  `Nurse_Notes` varchar(1000) NOT NULL,
+  `Diagnosis` varchar(1000) NOT NULL,
+  `Treatment_Plan` varchar(1000) NOT NULL,
+  `Treatment_History` varchar(1000) NOT NULL,
+  `Medications` varchar(1000) NOT NULL,
+  `Allergies` varchar(1000) NOT NULL,
+  `General_Info` varchar(1000) NOT NULL
+);
